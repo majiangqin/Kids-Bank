e8a3da6228366f2bd1f6d2ab774de45acdf20c92
Jiangqin Ma <jma416@gatech.edu>
2024-05-01 21:29:26 -0700

    modify files

--- app.py
+++ app.py
@@ -65,6 +65,64 @@ def translate_text(text, language_code):
 #             }]
 #         }
 #     })
+### Define a Route
+@app.route('/my_webhook', methods=['POST'])
+### Define the function that will be executed when the associated route is called
+
+def post_webhook_dialogflow():
+    # 1) Getting information from dialogflow agent request
+    body = request.get_json(silent=True)
+
+    # Get tag used to identify which fulfillment is being called.
+    fulfillment = body['fulfillmentInfo']['tag']
+
+    # Get parameters that are required to handle the desired action
+    prameters = []
+    for key, value in body['sessionInfo']['parameters'].items():
+        prameters.append({'name': key, 'value': value})
+
+    # 2) Execute action
+    msg = invoke_action(fulfillment, prameters)
+
+    # 3) provide a webhook Response to the Dialogflow Agent
+    WebhookResponse = answer_webhook(msg)
+    return WebhookResponse
+
+
+### Exploit parameters and incorporate them in the text response
+def invoke_action(fulfillment, prameters):
+    print("\n\n\n\n\n=========> CALL API ",fulfillment)
+    if fulfillment == "GetWeather_fulfillment":
+        for prameter in prameters:
+             if prameter['name']=="city":
+                 city=str(prameter['value'])
+        appid="25e5d7b2fff948d0749a8b9e9e14f5f9"
+        url = 'http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid='+appid
+        result = requests.get(url)
+        jsonResult = result.json()
+        if result.status_code == 200:
+            weatherCondition = jsonResult['weather'][0]['description']
+            reply = "There is {} there.".format(weatherCondition)
+            print(reply)
+            return reply
+        else:
+            return "Something wrong with the API."
+
+
+#### Processes the webhook answer which should follow a particular JSON format
+def answer_webhook(msg):
+    message = {"fulfillment_response": {
+
+        "messages": [
+            {
+                "text": {
+                    "text": [msg]
+                }
+            }
+        ]
+    }
+    }
+    return Response(json.dumps(message), 200, mimetype='application/json')
 
 
 # Route to serve the HTML page
